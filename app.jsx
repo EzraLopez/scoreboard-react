3563a6babaf8c718c9cd93a4f0b58723b7fbaa84
Esdras Lopez <5963060+EzraLopez@users.noreply.github.com>
2018-03-26 21:21:30 -0500

    Add Stopwatch component

--- app.jsx
+++ app.jsx
@@ -11,6 +11,68 @@ var PLAYERS = [
   },
 ];
 var nextId = PLAYERS.length;
+
+var Stopwatch = React.createClass({
+  getInitialState: function() {
+    return {
+      running: false,
+      elapsedTime: 0,
+      previousTime: 0,
+    }
+  },
+
+  componentDidMount: function() {
+    this.interval = setInterval(this.onTick, 100);
+  },
+
+  componentWillUnmount: function() {
+    clearInterval(this.interval);
+  },
+
+  onTick: function() {
+    if (this.state.running) {
+      var now = Date.now();
+      this.setState({
+        previousTime: now,
+        elapsedTime: this.state.elapsedTime + (now - this.state.previousTime),
+      });
+    }
+  },
+
+  onStart: function() {
+    this.setState({
+      running: true,
+      previousTime: Date.now(),
+    })
+  },
+
+  onStop: function() {
+    this.setState({running: false})
+  },
+
+  onReset: function() {
+    this.setState({
+      elapsedTime: 0,
+      previousTime: Date.now(),
+    })
+  },
+  
+  render: function() {
+    var seconds = Math.floor(this.state.elapsedTime/1000);
+    return (
+      <div className="stopwatch">
+        <h2>Stopwatch</h2>
+        <div className="stopwatch-time">{seconds}</div>
+        { this.state.running ? 
+          <button onClick={this.onStop}>Stop</button> 
+          :
+          <button onClick={this.onStart}>Start</button> 
+        }
+        <button onClick={this.onReset}>Reset</button>
+      </div>
+    )
+  }
+});
   
 var AddPlayerForm = React.createClass({
   propTypes: {
@@ -79,6 +141,7 @@ function Header(props) {
     <div className="header">
       <Stats players={props.players}/>
       <h1>{props.title}</h1>
+      <Stopwatch />
     </div>
   );
 }
@@ -187,6 +250,6 @@ var Application = React.createClass({
       </div>
     );
   }
-});  
+});   
 
 ReactDOM.render(<Application initialPlayers={PLAYERS}/>, document.getElementById('container'));
